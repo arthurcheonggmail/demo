5400925467465dd74da81cf4e04e44caab043eb5
arthurcheonggmail <arthurcheong@gmail.com>
2026-06-26 18:16:21 +0800

    feat: Add introductory line to test commit and push feature

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! Test");
         System.out.println("New Changed Line");
+        System.out.println("Add 1 more Changed Line");
     }
 }
